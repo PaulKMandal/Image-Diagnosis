e0987b8b433367411228cde850edb8c615ce6395
Brendon  Barrios <40436651+BrendosaurusRex@users.noreply.github.com>
2018-08-21 14:00:16 -0700

    Add files via upload

---
+++ Run_Console.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+/fastai/Image-Diagnosis/bin/git pull
+/fastai/Image-Diagnosis/bin/python /fastai/Image-Diagnosis/Paperspace_Notebook.py
+/fastai/Image-Diagnosis/bin/Paperspace_Notebook.py>output.txt
+
+
+
